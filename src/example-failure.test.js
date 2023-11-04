be12bbc729a553f783c1a97a2be91dfccdcf429b
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-11-04 17:35:35 +0100

    unit testing with example functions

---
+++ src/example-failure.test.js
@@ -0,0 +1,14 @@
+import getExampleData from "./example.js";
+
+// Testing failure
+const mockFetchFailure = jest.fn().mockResolvedValue({
+  ok: false,
+});
+
+global.fetch = mockFetchFailure;
+
+describe("getExampleData", () => {
+  it("throws an error when the request fails", async () => {
+    await expect(getExampleData(3)).rejects.toThrow("Unable to fetch data");
+  });
+});
